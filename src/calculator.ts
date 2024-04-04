a5018105e6cc77cc9e9ce81f28a0a51c06e2c234
UmeshGayashan <umeshgayashan099@gmail.com>
2024-04-04 20:29:35 +0530

    Created first test shoot and First test case (3A)

--- src/calculator.ts
+++ src/calculator.ts
@@ -15,3 +15,4 @@ class calculator{
         return a/b;
     }
 }
+export default calculator;
---
+++ test/calculator.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import Calculator from '../src/calculator'
+//First Test Shoot
+describe('Test Calculator Class', ()=>{
+
+    // First test case
+    it('should return sum',()=>{
+        // 3A Pattern used to Write a test case
+
+        // Arrange
+        // Create instance for calculator class
+        const calc = new Calculator();
+
+        // Act
+        const result = calc.add(2,3);
+
+        // Assert
+        expect(result).to.equal(5);
+    })
+})
